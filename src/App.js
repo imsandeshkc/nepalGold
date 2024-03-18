ac12ce603a11b981fb143ef3bc3330f0f87428a4
imsandeshkc <chhetrisandesh123@gmail.com>
2024-03-18 16:08:22 +0545

    banner.jsx content wrote

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './app.css'
+import Banner from './components/Home/Banner/Banner'
 
 const App = () => {
   return (
     <>
-      
+      <Banner/>
     </>
   )
 }
---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './banner.css'
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+        <span className='banner-tel'>
+            Tel +977-1-4532270
+        </span>
+        <span className='banner-email'>
+            Email info@fenegosida.org
+        </span>
+    </div>
+  )
+}
+
+export default Banner
