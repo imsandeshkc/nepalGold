c8e9850ef8f5d67c3ac9d6cfdccb4f1b6cf8cb39
imsandeshkc <chhetrisandesh123@gmail.com>
2024-03-21 13:54:53 +0545

    navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,43 @@
+import { useRef, useState } from 'react';
+
 import './navbar.css';
 
 import logo from "../../assets/logo of nepalgold.png";
 
 import { MdOutlineArrowDropDown } from "react-icons/md";
 
+
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const Drop1 = ['Organizational Structure', 'Types of Membership', 'Introduction', 'Province Committee', 'Executive Committee', 'Message from President'];
+
+    // solution
+    
+    const drop1Ref = useRef();
+    const div1Ref = useRef();
+    
+    window.addEventListener('click', (e) => {
+        if (e.target !== drop1Ref.current && e.target !== div1Ref.current){
+            setOpen(false);
+        }
+    })
+    
+    const Drop2 = ['Affiliated Members', 'Associated Members'];
+
+    // solution
+    
+    const drop2Ref = useRef();
+    const div2Ref = useRef();
+    
+    window.addEventListener('click', (e) => {
+        if (e.target !== drop2Ref.current && e.target !== div2Ref.current){
+            setOpen(false);
+        }
+    })
+
+
   return (
     <div className="header">
 
@@ -26,29 +59,61 @@ const Navbar = () => {
 
                 <li className='navItems'>
 
-                    <a href='1' className='navLink'>About US<MdOutlineArrowDropDown className='icon'/></a>
-
-                    <div className='dropdown_btn'>
-                        <ul>
-                            <li><a href='1'>Organizational Structure</a></li>
-                            <li><a href="1">Types of Membership</a></li>
-                            <li><a href="1">Introduction</a></li>
-                            <li><a href="1">Province Committee</a></li>
-                            <li><a href="1">Executive Committee</a></li>
-                            <li><a href="1">Message from President</a></li>
-                        </ul>
+                    <div
+                    ref={div1Ref}
+                    onClick={() => setOpen(!open)} 
+                    href='1'
+                    className='navLink'>
+                        About US
+                        <MdOutlineArrowDropDown className='icon'/>
                     </div>
+
+                    {
+                        open &&
+
+                        <div ref={drop1Ref} className='dropdown_btn'>
+                            <ul>
+                                {Drop1.map((drop) => (
+                                    <li 
+                                        onClick={() => setOpen(false)}
+                                        className='dropdown_li' 
+                                        key={drop}>
+                                            {drop}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    }  
 
                 </li>
 
                 <li className='navItems'>
 
-                    <a href='1' className='navLink'>Members<MdOutlineArrowDropDown className='icon'/></a>
+                    <div 
+                    ref={div2Ref}
+                    onClick={() => setOpen(!open)}
+                    href='1'
+                    className='navLink'>
+                        Members
+                        <MdOutlineArrowDropDown className='icon'/>
+                    </div>
+                
+                    {
+                        open &&
 
-                    <ul className='dropdown_btn'>
-                        <li><a href='1'>Affiliated Members</a></li>
-                        <li><a href="1">Associated Members</a></li>
-                    </ul>
+                        <div ref={drop2Ref} className="dropdown_btn">
+                            <ul>
+                                {Drop2.map((drop) => (
+                                    <li
+                                        onClick={() => setOpen(false)}
+                                        className='dropdown_li'
+                                        key={drop}>
+                                            {drop}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    }
 
                 </li>
 
@@ -56,10 +121,12 @@ const Navbar = () => {
 
                     <a href='1' className='navLink'>Rates<MdOutlineArrowDropDown className='icon'/></a>
 
-                    <ul className='dropdown_btn'>
-                        <li><a href='1'>History</a></li>
-                        <li><a href="1">Graph</a></li>
-                    </ul>
+                    {/* <div className='dropdown_btn'>
+                        <ul>
+                            <li><a href='1'>History</a></li>
+                            <li><a href="1">Graph</a></li>
+                        </ul>
+                    </div> */}
 
                 </li>
 
@@ -67,16 +134,18 @@ const Navbar = () => {
 
                     <a href='1' className='navLink'>Training<MdOutlineArrowDropDown className='icon'/></a>
 
-                    <ul className='dropdown_btn'>
-                        <li><a href='1'>गरगहना बनाउने तालीम ६ महिना</a></li>
-                        <li><a href="1">सुन जाँचकी तालीम १ महिना</a></li>
-                        <li><a href="1">पत्थर सेटिंग तालीम १ महिना</a></li>
-                        <li><a href="1">सुन रिफाइन गर्ने तालीम १ महिना</a></li>
-                        <li><a href="1">गरगहना पालिश गर्ने तालीम १ महिना</a></li>
-                        <li><a href="1">सिक्री बनाउने तालीम ः ३ महिना</a></li>
-                        <li><a href="1">गरगहना डिजाइन गर्ने तालीम ३ महिना</a></li>
-                        <li><a href="1">बुट्टा काट्ने तालीम ३ महिना</a></li>
-                    </ul>
+                    {/* <div className="dropdown_btn">
+                        <ul>
+                            <li><a href='1'>गरगहना बनाउने तालीम ६ महिना</a></li>
+                            <li><a href="1">सुन जाँचकी तालीम १ महिना</a></li>
+                            <li><a href="1">पत्थर सेटिंग तालीम १ महिना</a></li>
+                            <li><a href="1">सुन रिफाइन गर्ने तालीम १ महिना</a></li>
+                            <li><a href="1">गरगहना पालिश गर्ने तालीम १ महिना</a></li>
+                            <li><a href="1">सिक्री बनाउने तालीम ः ३ महिना</a></li>
+                            <li><a href="1">गरगहना डिजाइन गर्ने तालीम ३ महिना</a></li>
+                            <li><a href="1">बुट्टा काट्ने तालीम ३ महिना</a></li>
+                        </ul>
+                    </div> */}
 
                 </li>
 
@@ -84,11 +153,13 @@ const Navbar = () => {
 
                     <a href='1' className='navLink'>Downloads<MdOutlineArrowDropDown className='icon'/></a>
 
-                    <ul className='dropdown_btn'>
-                        <li><a href='1'>Forms</a></li>
-                        <li><a href="1">Bulletin</a></li>
-                        <li><a href="1">Weekly Reports</a></li>
-                    </ul>
+                    {/* <div className="dropdown_btn">
+                        <ul>
+                            <li><a href='1'>Forms</a></li>
+                            <li><a href="1">Bulletin</a></li>
+                            <li><a href="1">Weekly Reports</a></li>
+                        </ul>
+                    </div> */}
 
                 </li>
 
@@ -96,15 +167,17 @@ const Navbar = () => {
 
                     <a href='1' className='navLink'>News<MdOutlineArrowDropDown className='icon'/></a>
 
-                    <ul className='dropdown_btn'>
-                        <li><a href='1' >Recent News</a></li>
-                        <li><a href="1">Notices</a></li>
-                        <li><a href="1">Press Release</a></li>
-                        <li><a href="1">Branch News</a></li>
-                        <li><a href="1">Photo Gallery</a></li>
-                        <li><a href="1">Video Gallery</a></li>
-                    </ul>
-
+                    {/* <div className="dropdown_btn">
+                        <ul>
+                            <li><a href='1' >Recent News</a></li>
+                            <li><a href="1">Notices</a></li>
+                            <li><a href="1">Press Release</a></li>
+                            <li><a href="1">Branch News</a></li>
+                            <li><a href="1">Photo Gallery</a></li>
+                            <li><a href="1">Video Gallery</a></li>
+                        </ul>
+                    </div> */}
+                    
                 </li>
 
                 <li className='navItems'>
